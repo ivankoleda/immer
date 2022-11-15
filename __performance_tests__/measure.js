9b9ab3a04877223ded2c9c24afa4bb02bcd16e3e
Ivan Kaliada <26203362+ivankoleda@users.noreply.github.com>
2022-11-15 14:03:27 +0000

    perf tests for spreading proxy

--- __performance_tests__/measure.js
+++ __performance_tests__/measure.js
@@ -1,20 +1,50 @@
 "use strict"
 
+function formatBytes(bytes, decimals = 2) {
+	if (!+bytes) return "0 Bytes"
+
+	const k = 1024
+	const dm = decimals < 0 ? 0 : decimals
+	const sizes = ["Bytes", "KB", "MB", "GB", "TB", "PB", "EB", "ZB", "YB"]
+
+	const i = Math.floor(Math.log(bytes) / Math.log(k))
+
+	return `${parseFloat((bytes / Math.pow(k, i)).toFixed(dm))} ${sizes[i]}`
+}
+
+global.gc && global.gc()
+
 function measureTime(setup, fn) {
-    if (!fn) {
-        fn = setup
-        setup = () => {}
-    }
-    const args = setup()
-    global.gc && global.gc()
-    const startTime = Date.now()
-    fn(args)
-    const endTime = Date.now()
-    return endTime - startTime
+	if (!fn) {
+		fn = setup
+		setup = () => {}
+	}
+	const args = setup()
+	const startTime = Date.now()
+	fn(args)
+	process.memoryUsage().heapUsed
+	const endTime = Date.now()
+
+	const beforeGcTime = Date.now()
+	const heapUsed = process.memoryUsage().heapUsed
+	global.gc && global.gc()
+
+	return {
+		time: endTime - startTime,
+		heapUsed,
+		gcTime: Date.now() - beforeGcTime
+	}
 }
 
 export function measure(name, setup, fn) {
-    const times = [...Array(5)].map(() => measureTime(setup, fn))
-    const medianTime = times.sort()[Math.round(times.length / 2)]
-    console.log(`${name}: ${medianTime}ms`)
+	const times = [...Array(5)].map(() => measureTime(setup, fn))
+	const medianExecution = times.sort((a, b) => a.time - b.time)[
+		Math.round(times.length / 2)
+	]
+
+	console.log(
+		`${name}:\n time: ${medianExecution.time}ms\n GC took ${
+			medianExecution.gcTime
+		}ms\n heapUsed: ${formatBytes(medianExecution.heapUsed)}`
+	)
 }
--- __performance_tests__/todo.js
+++ __performance_tests__/todo.js
@@ -18,7 +18,7 @@ function freeze(x) {
 	return x
 }
 
-const MAX = 50000
+const MAX = 100000
 const MODIFY_FACTOR = 0.1
 const baseState = []
 let frozenBazeState
@@ -32,6 +32,12 @@ for (let i = 0; i < MAX; i++) {
 		done: false,
 		someThingCompletelyIrrelevant: [1, 2, 3, 4, 5, 6, 7, 8, 9, 0]
 	})
+}
+
+const objectState = {
+	first: {foo: "bar"},
+	second: {foo: "baz"},
+	third: {foo: "barbaz"}
 }
 
 // Produce the frozen bazeState
@@ -50,257 +56,317 @@ seamlessBaseState = Seamless.from(baseState)
 
 console.log("\n# todo - performance\n")
 
-measure(
-	"just mutate",
-	() => ({draft: cloneDeep(baseState)}),
-	({draft}) => {
-		for (let i = 0; i < MAX * MODIFY_FACTOR; i++) {
-			draft[i].done = true
-		}
-	}
-)
+// measure(
+// 	"just mutate",
+// 	() => ({draft: cloneDeep(baseState)}),
+// 	({draft}) => {
+// 		for (let i = 0; i < MAX * MODIFY_FACTOR; i++) {
+// 			draft[i].done = true
+// 		}
+// 	}
+// )
+
+// measure(
+// 	"just mutate, freeze",
+// 	() => ({draft: cloneDeep(baseState)}),
+// 	({draft}) => {
+// 		for (let i = 0; i < MAX * MODIFY_FACTOR; i++) {
+// 			draft[i].done = true
+// 		}
+// 		deepFreeze(draft)
+// 	}
+// )
+
+// measure("deepclone, then mutate", () => {
+// 	const draft = cloneDeep(baseState)
+// 	for (let i = 0; i < MAX * MODIFY_FACTOR; i++) {
+// 		draft[i].done = true
+// 	}
+// })
+
+// measure("deepclone, then mutate, then freeze", () => {
+// 	const draft = cloneDeep(baseState)
+// 	for (let i = 0; i < MAX * MODIFY_FACTOR; i++) {
+// 		draft[i].done = true
+// 	}
+// 	deepFreeze(draft)
+// })
+
+// measure("handcrafted reducer (no freeze)", () => {
+// 	const nextState = [
+// 		...baseState.slice(0, MAX * MODIFY_FACTOR).map(todo => ({
+// 			...todo,
+// 			done: true
+// 		})),
+// 		...baseState.slice(MAX * MODIFY_FACTOR)
+// 	]
+// })
+
+// measure("handcrafted reducer (with freeze)", () => {
+// 	const nextState = freeze([
+// 		...baseState.slice(0, MAX * MODIFY_FACTOR).map(todo =>
+// 			freeze({
+// 				...todo,
+// 				done: true
+// 			})
+// 		),
+// 		...baseState.slice(MAX * MODIFY_FACTOR)
+// 	])
+// })
+
+// measure("naive handcrafted reducer (without freeze)", () => {
+// 	const nextState = baseState.map((todo, index) => {
+// 		if (index < MAX * MODIFY_FACTOR)
+// 			return {
+// 				...todo,
+// 				done: true
+// 			}
+// 		else return todo
+// 	})
+// })
+
+// measure("naive handcrafted reducer (with freeze)", () => {
+// 	const nextState = deepFreeze(
+// 		baseState.map((todo, index) => {
+// 			if (index < MAX * MODIFY_FACTOR)
+// 				return {
+// 					...todo,
+// 					done: true
+// 				}
+// 			else return todo
+// 		})
+// 	)
+// })
+
+// measure("immutableJS", () => {
+// 	let state = immutableJsBaseState
+// 	state.withMutations(state => {
+// 		for (let i = 0; i < MAX * MODIFY_FACTOR; i++) {
+// 			state.setIn([i, "done"], true)
+// 		}
+// 	})
+// })
+
+// measure("immutableJS + toJS", () => {
+// 	let state = immutableJsBaseState
+// 		.withMutations(state => {
+// 			for (let i = 0; i < MAX * MODIFY_FACTOR; i++) {
+// 				state.setIn([i, "done"], true)
+// 			}
+// 		})
+// 		.toJS()
+// })
+
+// measure("seamless-immutable", () => {
+// 	const state = seamlessBaseState
+// 	state.map((todo, index) => {
+// 		if (index < MAX * MODIFY_FACTOR) return todo.set("done", true)
+// 		else return todo
+// 	})
+// })
+
+// measure("seamless-immutable + asMutable", () => {
+// 	const state = seamlessBaseState
+// 	state
+// 		.map((todo, index) => {
+// 			if (index < MAX * MODIFY_FACTOR) return todo.set("done", true)
+// 			else return todo
+// 		})
+// 		.asMutable({deep: true})
+// })
+
+// measure(
+// 	"immer (proxy) - without autofreeze",
+// 	() => {
+// 		setUseProxies(true)
+// 		setAutoFreeze(false)
+// 	},
+// 	() => {
+// 		produce(baseState, draft => {
+// 			for (let i = 0; i < MAX * MODIFY_FACTOR; i++) {
+// 				draft[i].done = true
+// 			}
+// 		})
+// 	}
+// )
+
+// measure(
+// 	"immer (proxy) - with autofreeze",
+// 	() => {
+// 		setUseProxies(true)
+// 		setAutoFreeze(true)
+// 	},
+// 	() => {
+// 		produce(frozenBazeState, draft => {
+// 			for (let i = 0; i < MAX * MODIFY_FACTOR; i++) {
+// 				draft[i].done = true
+// 			}
+// 		})
+// 	}
+// )
+
+// measure(
+// 	"immer (proxy) - without autofreeze - with patch listener",
+// 	() => {
+// 		setUseProxies(true)
+// 		setAutoFreeze(false)
+// 	},
+// 	() => {
+// 		produce(
+// 			baseState,
+// 			draft => {
+// 				for (let i = 0; i < MAX * MODIFY_FACTOR; i++) {
+// 					draft[i].done = true
+// 				}
+// 			},
+// 			function() {}
+// 		)
+// 	}
+// )
+
+// measure(
+// 	"immer (proxy) - with autofreeze - with patch listener",
+// 	() => {
+// 		setUseProxies(true)
+// 		setAutoFreeze(true)
+// 	},
+// 	() => {
+// 		produce(
+// 			baseState,
+// 			draft => {
+// 				for (let i = 0; i < MAX * MODIFY_FACTOR; i++) {
+// 					draft[i].done = true
+// 				}
+// 			},
+// 			function() {}
+// 		)
+// 	}
+// )
+
+// measure(
+// 	"immer (es5) - without autofreeze",
+// 	() => {
+// 		setUseProxies(false)
+// 		setAutoFreeze(false)
+// 	},
+// 	() => {
+// 		produce(baseState, draft => {
+// 			for (let i = 0; i < MAX * MODIFY_FACTOR; i++) {
+// 				draft[i].done = true
+// 			}
+// 		})
+// 	}
+// )
+
+// measure(
+// 	"immer (es5) - with autofreeze",
+// 	() => {
+// 		setUseProxies(false)
+// 		setAutoFreeze(true)
+// 	},
+// 	() => {
+// 		produce(frozenBazeState, draft => {
+// 			for (let i = 0; i < MAX * MODIFY_FACTOR; i++) {
+// 				draft[i].done = true
+// 			}
+// 		})
+// 	}
+// )
+
+// measure(
+// 	"immer spreading a lot of items",
+// 	() => {
+// 		// setUseProxies(false)
+// 		// setAutoFreeze(false)
+// 	},
+// 	() => {
+// 		return baseState.map(item => produce(baseState, draft => {
+// 			return {
+// 				...draft,
+// 				foo: 'bar'
+// 			}
+// 		}))
+// 	}
+// )
+
+// measure(
+// 	"immer (es5) - without autofreeze - with patch listener",
+// 	() => {
+// 		setUseProxies(false)
+// 		setAutoFreeze(false)
+// 	},
+// 	() => {
+// 		produce(
+// 			baseState,
+// 			draft => {
+// 				for (let i = 0; i < MAX * MODIFY_FACTOR; i++) {
+// 					draft[i].done = true
+// 				}
+// 			},
+// 			function() {}
+// 		)
+// 	}
+// )
+
+// measure(
+// 	"immer (es5) - with autofreeze - with patch listener",
+// 	() => {
+// 		setUseProxies(false)
+// 		setAutoFreeze(true)
+// 	},
+// 	() => {
+// 		produce(
+// 			baseState,
+// 			draft => {
+// 				for (let i = 0; i < MAX * MODIFY_FACTOR; i++) {
+// 					draft[i].done = true
+// 				}
+// 			},
+// 			function() {}
+// 		)
+// 	}
+// )
 
 measure(
-	"just mutate, freeze",
-	() => ({draft: cloneDeep(baseState)}),
-	({draft}) => {
-		for (let i = 0; i < MAX * MODIFY_FACTOR; i++) {
-			draft[i].done = true
-		}
-		deepFreeze(draft)
-	}
-)
-
-measure("deepclone, then mutate", () => {
-	const draft = cloneDeep(baseState)
-	for (let i = 0; i < MAX * MODIFY_FACTOR; i++) {
-		draft[i].done = true
-	}
-})
-
-measure("deepclone, then mutate, then freeze", () => {
-	const draft = cloneDeep(baseState)
-	for (let i = 0; i < MAX * MODIFY_FACTOR; i++) {
-		draft[i].done = true
-	}
-	deepFreeze(draft)
-})
-
-measure("handcrafted reducer (no freeze)", () => {
-	const nextState = [
-		...baseState.slice(0, MAX * MODIFY_FACTOR).map(todo => ({
-			...todo,
-			done: true
-		})),
-		...baseState.slice(MAX * MODIFY_FACTOR)
-	]
-})
-
-measure("handcrafted reducer (with freeze)", () => {
-	const nextState = freeze([
-		...baseState.slice(0, MAX * MODIFY_FACTOR).map(todo =>
-			freeze({
-				...todo,
-				done: true
-			})
-		),
-		...baseState.slice(MAX * MODIFY_FACTOR)
-	])
-})
-
-measure("naive handcrafted reducer (without freeze)", () => {
-	const nextState = baseState.map((todo, index) => {
-		if (index < MAX * MODIFY_FACTOR)
-			return {
-				...todo,
-				done: true
-			}
-		else return todo
-	})
-})
-
-measure("naive handcrafted reducer (with freeze)", () => {
-	const nextState = deepFreeze(
-		baseState.map((todo, index) => {
-			if (index < MAX * MODIFY_FACTOR)
+	"using spread operator with immer to add a new property",
+	() => {},
+	() => {
+		return baseState.map(item =>
+			produce(item, draft => {
 				return {
-					...todo,
-					done: true
+					...draft,
+					foo: "bar"
 				}
-			else return todo
-		})
-	)
-})
-
-measure("immutableJS", () => {
-	let state = immutableJsBaseState
-	state.withMutations(state => {
-		for (let i = 0; i < MAX * MODIFY_FACTOR; i++) {
-			state.setIn([i, "done"], true)
-		}
-	})
-})
-
-measure("immutableJS + toJS", () => {
-	let state = immutableJsBaseState
-		.withMutations(state => {
-			for (let i = 0; i < MAX * MODIFY_FACTOR; i++) {
-				state.setIn([i, "done"], true)
-			}
-		})
-		.toJS()
-})
-
-measure("seamless-immutable", () => {
-	const state = seamlessBaseState
-	state.map((todo, index) => {
-		if (index < MAX * MODIFY_FACTOR) return todo.set("done", true)
-		else return todo
-	})
-})
-
-measure("seamless-immutable + asMutable", () => {
-	const state = seamlessBaseState
-	state
-		.map((todo, index) => {
-			if (index < MAX * MODIFY_FACTOR) return todo.set("done", true)
-			else return todo
-		})
-		.asMutable({deep: true})
-})
-
-measure(
-	"immer (proxy) - without autofreeze",
-	() => {
-		setUseProxies(true)
-		setAutoFreeze(false)
-	},
-	() => {
-		produce(baseState, draft => {
-			for (let i = 0; i < MAX * MODIFY_FACTOR; i++) {
-				draft[i].done = true
-			}
-		})
-	}
-)
-
-measure(
-	"immer (proxy) - with autofreeze",
-	() => {
-		setUseProxies(true)
-		setAutoFreeze(true)
-	},
-	() => {
-		produce(frozenBazeState, draft => {
-			for (let i = 0; i < MAX * MODIFY_FACTOR; i++) {
-				draft[i].done = true
-			}
-		})
-	}
-)
-
-measure(
-	"immer (proxy) - without autofreeze - with patch listener",
-	() => {
-		setUseProxies(true)
-		setAutoFreeze(false)
-	},
-	() => {
-		produce(
-			baseState,
-			draft => {
-				for (let i = 0; i < MAX * MODIFY_FACTOR; i++) {
-					draft[i].done = true
-				}
-			},
-			function() {}
+			})
 		)
 	}
 )
 
+global.gc()
+
 measure(
-	"immer (proxy) - with autofreeze - with patch listener",
+	"adding a new property to immer proxy in a 'mutable'/intended way",
+	() => {},
 	() => {
-		setUseProxies(true)
-		setAutoFreeze(true)
-	},
-	() => {
-		produce(
-			baseState,
-			draft => {
-				for (let i = 0; i < MAX * MODIFY_FACTOR; i++) {
-					draft[i].done = true
-				}
-			},
-			function() {}
+		return baseState.map(item =>
+			produce(item, draft => {
+				draft.foo = "bar"
+			})
 		)
 	}
 )
 
+global.gc()
+
 measure(
-	"immer (es5) - without autofreeze",
+	"adding new property via spread for regular js object",
+	() => {},
 	() => {
-		setUseProxies(false)
-		setAutoFreeze(false)
-	},
-	() => {
-		produce(baseState, draft => {
-			for (let i = 0; i < MAX * MODIFY_FACTOR; i++) {
-				draft[i].done = true
-			}
-		})
+		return baseState.map(item => ({
+			...item,
+			foo: "bar"
+		}))
 	}
 )
 
-measure(
-	"immer (es5) - with autofreeze",
-	() => {
-		setUseProxies(false)
-		setAutoFreeze(true)
-	},
-	() => {
-		produce(frozenBazeState, draft => {
-			for (let i = 0; i < MAX * MODIFY_FACTOR; i++) {
-				draft[i].done = true
-			}
-		})
-	}
-)
-
-measure(
-	"immer (es5) - without autofreeze - with patch listener",
-	() => {
-		setUseProxies(false)
-		setAutoFreeze(false)
-	},
-	() => {
-		produce(
-			baseState,
-			draft => {
-				for (let i = 0; i < MAX * MODIFY_FACTOR; i++) {
-					draft[i].done = true
-				}
-			},
-			function() {}
-		)
-	}
-)
-
-measure(
-	"immer (es5) - with autofreeze - with patch listener",
-	() => {
-		setUseProxies(false)
-		setAutoFreeze(true)
-	},
-	() => {
-		produce(
-			baseState,
-			draft => {
-				for (let i = 0; i < MAX * MODIFY_FACTOR; i++) {
-					draft[i].done = true
-				}
-			},
-			function() {}
-		)
-	}
-)
+global.gc()
